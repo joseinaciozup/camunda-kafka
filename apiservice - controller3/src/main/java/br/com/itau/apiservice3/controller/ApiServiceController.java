507bc6ee5db5eb398922d59bfd572e9661c26d44
wesly.borges <wesly.borges@zup.com.br>
2020-01-14 11:29:13 -0300

    Criando responseEntity

--- apiservice - controller3/src/main/java/br/com/itau/apiservice3/controller/ApiServiceController.java
+++ apiservice - controller3/src/main/java/br/com/itau/apiservice3/controller/ApiServiceController.java
@@ -2,6 +2,8 @@ package br.com.itau.apiservice3.controller;
 
 import java.util.Random;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiServiceController {
 
     @GetMapping
-    public String execService() throws InterruptedException {
+    public ResponseEntity<?> execService() throws InterruptedException {
 
         Integer valorExecution = new Random().nextInt(6);
         if (valorExecution > 0){
             Thread.sleep((valorExecution * 1000));
 
-            return "Executando a API service 3";
+            return new ResponseEntity("Executando a API service 3", HttpStatus.OK);
         }else
-            throw new RuntimeException("Ocorreu um erro na execução do serviço API service 3");
+            return new ResponseEntity("Ocorreu um erro na execução do serviço API service 3", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
