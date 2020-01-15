2b305bd22825da5426413e9188a6ca598e286560
wesly.borges <wesly.borges@zup.com.br>
2020-01-15 16:33:00 -0300

    criando processo de falha

--- worker-controller-2/src/main/java/br/com/itau/worker/controller/WorkerController.java
+++ worker-controller-2/src/main/java/br/com/itau/worker/controller/WorkerController.java
@@ -1,8 +1,13 @@
 package br.com.itau.worker.controller;
 
-import br.itau.journey.camunda.rest.feign.dto.RequestControllerStartDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.itau.journey.camunda.rest.feign.dto.RequestControllerStartDTO;
 
 @RestController
 @RequestMapping("worker-controller-2")
@@ -10,7 +15,12 @@ public class WorkerController {
 
     @PostMapping
     public ResponseEntity<?> execService(@RequestBody RequestControllerStartDTO request) throws InterruptedException {
-        Thread.sleep(Long.parseLong(request.getTime()));
+
+        if (Integer.parseInt(request.getTime()) > 0){
+            Thread.sleep(Long.parseLong(request.getTime()));
+        }else
+            return new ResponseEntity("Ocorreu um erro no work-controller-1", HttpStatus.INTERNAL_SERVER_ERROR);
+
         return ResponseEntity.ok(request);
     }
 }
